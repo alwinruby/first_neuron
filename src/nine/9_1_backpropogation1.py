340d8f42ec28e8247bf8ed40b94387824665d586
alwinruby <alwinsolanky@gmail.com>
2022-02-18 00:16:58 +0000

    backpropogation

---
+++ src/nine/9_1_backpropogation1.py
@@ -0,0 +1,5 @@
+x = [1.0, -2.0, 3.0] # input values
+w = [-3.0, -1.0, 2.0] # weights
+b = 1.0 # bias
+xw0 = x[0] * w[0]
+print(xw0)
---
+++ src/nine/9_2_backpropogation2.py
@@ -0,0 +1,7 @@
+x = [1.0, -2.0, 3.0] # input values
+w = [-3.0, -1.0, 2.0] # weights
+b = 1.0 # bias
+
+xw1 = x[1] * w[1]
+xw2 = x[2] * w[2]
+print(xw1, xw2)
