ef93f015e75b9f69a689abdee4fbf9aedc586a01
alwinruby <alwinsolanky@gmail.com>
2022-02-19 00:40:04 +0000

    backpropogation together

---
+++ src/nine/9_3_backpropogation3.py
@@ -0,0 +1,11 @@
+# Forward pass
+x = [1.0, -2.0, 3.0] # input values
+w = [-3.0, -1.0, 2.0] # weights
+b = 1.0 # bias
+
+
+# Multiplying inputs by weights
+xw0 = x[0] * w[0]
+xw1 = x[1] * w[1]
+xw2 = x[2] * w[2]
+print(xw0, xw1, xw2)
