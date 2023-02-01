b89734d474722c023315c6470b56f82bdc81e903
alwinruby <alwinsolanky@gmail.com>
2023-02-01 05:11:50 +0000

    17_12 to Activation_Sigmoid

--- src/seventeen/17_12_full_code.py
+++ src/seventeen/17_12_full_code.py
@@ -100,35 +100,42 @@ class Activation_ReLU:
 
 # Softmax activation
 class Activation_Softmax:
-# Forward pass
-def forward(self, inputs):
-# Remember input values
-self.inputs = inputs
-# Get unnormalized probabilities
-exp_values = np.exp(inputs - np.max(inputs, axis=1,
-keepdims=True))
-# Normalize them for each sample
-probabilities = exp_values / np.sum(exp_values, axis=1,
-keepdims=True)
-self.output = probabilities
-# Backward pass
-def backward(self, dvalues):
-# Create uninitialized array
-self.dinputs = np.empty_like(dvalues)
-# Enumerate outputs and gradients
-for index, (single_output, single_dvalues) in \
-enumerate(zip(self.output, dvalues)):
-# Flatten output array
-single_output = single_output.reshape(-1, 1)
-nnfs_S8532 win1857@yahoo.co.uk
-Chapter 17 | Regression | 461
-# Calculate Jacobian matrix of the output
-jacobian_matrix = np.diagflat(single_output) - \
-np.dot(single_output, single_output.T)
-# Calculate sample-wise gradient
-# and add it to the array of sample gradients
-self.dinputs[index] = np.dot(jacobian_matrix,
-single_dvalues)
+
+    # Forward pass
+    def forward(self, inputs):
+        # Remember input values
+        self.inputs = inputs
+
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                             keepdims=True))
+
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+                                            keepdims=True)
+        self.output = probabilities
+
+    # Backward pass
+    def backward(self, dvalues):
+
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - \
+                              np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix,
+            single_dvalues)
+
+
 # Sigmoid activation
 class Activation_Sigmoid:
 # Forward pass
