df047beeabeaaef873480d5588c171aefa632e7e
alwinruby <alwinsolanky@gmail.com>
2023-02-10 21:56:14 +0000

    Loss_BinaryCrossentropy(Loss)

--- src/seventeen/17_12_full_code.py
+++ src/seventeen/17_12_full_code.py
@@ -497,34 +497,46 @@ class Loss_CategoricalCrossentropy(Loss):
 # Softmax classifier - combined Softmax activation
 # and cross-entropy loss for faster backward step
 class Activation_Softmax_Loss_CategoricalCrossentropy():
-# Creates activation and loss function objects
-def __init__(self):
-self.activation = Activation_Softmax()
-self.loss = Loss_CategoricalCrossentropy()
-# Forward pass
-def forward(self, inputs, y_true):
-# Output layer's activation function
-self.activation.forward(inputs)
-# Set the output
-self.output = self.activation.output
-# Calculate and return loss value
-return self.loss.calculate(self.output, y_true)
-nnfs_S8532 win1857@yahoo.co.uk
-Chapter 17 | Regression | 469
-# Backward pass
-def backward(self, dvalues, y_true):
-# Number of samples
-samples = len(dvalues)
-# If labels are one-hot encoded,
-# turn them into discrete values
-if len(y_true.shape) == 2:
-y_true = np.argmax(y_true, axis=1)
-# Copy so we can safely modify
-self.dinputs = dvalues.copy()
-# Calculate gradient
-self.dinputs[range(samples), y_true] -= 1
-# Normalize gradient
-self.dinputs = self.dinputs / samples
+
+    # Creates activation and loss function objects
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossentropy()
+
+    # Forward pass
+    def forward(self, inputs, y_true):
+
+        # Output layer's activation function
+        self.activation.forward(inputs)
+
+        # Set the output
+        self.output = self.activation.output
+
+        # Calculate and return loss value
+        return self.loss.calculate(self.output, y_true)
+
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+
+        # If labels are one-hot encoded,
+        # turn them into discrete values
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        # Copy so we can safely modify
+        self.dinputs = dvalues.copy()
+
+        # Calculate gradient
+        self.dinputs[range(samples), y_true] -= 1
+
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+
 # Binary cross-entropy loss
 class Loss_BinaryCrossentropy(Loss):
 # Forward pass
