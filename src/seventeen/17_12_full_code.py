c08920631f55faafa7390757d4a6f8696b67cba9
alwinruby <alwinsolanky@gmail.com>
2023-02-11 08:51:32 +0000

    Loss_MeanSquaredError(Loss)

--- src/seventeen/17_12_full_code.py
+++ src/seventeen/17_12_full_code.py
@@ -539,33 +539,45 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
 
 # Binary cross-entropy loss
 class Loss_BinaryCrossentropy(Loss):
-# Forward pass
-def forward(self, y_pred, y_true):
-# Clip data to prevent division by 0
-# Clip both sides to not drag mean towards any value
-y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
-# Calculate sample-wise loss
-sample_losses = -(y_true * np.log(y_pred_clipped) +
-(1 - y_true) * np.log(1 - y_pred_clipped))
-sample_losses = np.mean(sample_losses, axis=-1)
-# Return losses
-return sample_losses
-# Backward pass
-def backward(self, dvalues, y_true):
-# Number of samples
-samples = len(dvalues)
-# Number of outputs in every sample
-# We'll use the first sample to count them
-outputs = len(dvalues[0])
-# Clip data to prevent division by 0
-# Clip both sides to not drag mean towards any value
-clipped_dvalues = np.clip(dvalues, 1e-7, 1 - 1e-7)
-470 | Neural Networks from Scratch
-# Calculate gradient
-self.dinputs = -(y_true / clipped_dvalues -
-(1 - y_true) / (1 - clipped_dvalues)) / outputs
-# Normalize gradient
-self.dinputs = self.dinputs / samples
+
+    # Forward pass
+    def forward(self, y_pred, y_true):
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Calculate sample-wise loss
+        sample_losses = -(y_true * np.log(y_pred_clipped) +
+                          (1 - y_true) * np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=-1)
+
+        # Return losses
+        return sample_losses
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+
+        # Number of outputs in every sample
+        # We'll use the first sample to count them
+        outputs = len(dvalues[0])
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        clipped_dvalues = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        # Calculate gradient
+        self.dinputs = -(y_true / clipped_dvalues -
+                         (1 - y_true) / (1 - clipped_dvalues)) / outputs
+
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+
+
 # Mean Squared Error loss
 class Loss_MeanSquaredError(Loss): # L2 loss
 # Forward pass
